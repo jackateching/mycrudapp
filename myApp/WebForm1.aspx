7f8915dfd765b4d760c5ea253d95cb8ca1f2b7c0
unknown <jackateching@gmail.com>
2021-07-24 20:56:07 -0400

    my first commit

---
+++ myApp/WebForm1.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="myApp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+   <link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="rowDiv">
+                <span style="width:15%">STUDENT ID</span>
+                <span style="width:20%">NAME</span>
+                <span style="width:20%">EMAIL</span> 
+                <span style="width:20%">OPTIONS</span> 
+            </div>
+        <div id="viewList" runat="server">
+            
+        </div>
+        <br />
+        <hr />
+        
+        <div class="addAndUpdate">
+            <asp:Label ID="sIDLabel" runat="server" Text="Student ID:"></asp:Label> 
+            <asp:Label ID="nameLabel" runat="server" Text="Name:"></asp:Label>
+            <asp:Label ID="emailLabel" runat="server" Text="Email:"></asp:Label>
+            <br />
+            <asp:TextBox ID="sIDTextBox" runat="server" ReadOnly="true" ></asp:TextBox>
+            <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+            <asp:TextBox ID="emailTextBox" runat="server"></asp:TextBox>
+            <asp:Button ID="addBtn" runat="server" Text="Add" OnClick="addBtn_Click" />
+            <asp:Button ID="updateBtn" runat="server" Text="Update" OnClick="updateBtn_Click" />
+        </div>
+
+    </form>
+
+    </body>
+</html>
+
---
+++ myApp/WebForm1.aspx.cs
@@ -0,0 +1,186 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Windows;
+
+namespace myApp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private static string connect_string = "DATA SOURCE=ORCL;PERSIST SECURITY INFO=True;USER ID=C##JACK;PASSWORD=jack";
+        OracleConnection conn = new OracleConnection(connect_string);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            load_grid_view();
+        }
+
+        private void load_grid_view()
+        {
+
+            OracleCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "STUDENT_PROCEDURE.GET_ALL_STUDENTS";
+
+            DataSet ds = new DataSet();
+            cmd.Parameters.Add(new OracleParameter("p_recordset", OracleDbType.RefCursor, ParameterDirection.Output));
+
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            da.Fill(ds, "DataTable1");
+
+            if (cmd.Connection.State != ConnectionState.Closed)
+                cmd.Connection.Close();
+
+            //GridView1.DataSource = ds.Tables[0]; //error occurs here
+            //GridView1.DataBind();
+            viewList.Controls.Clear();
+
+            foreach (DataTable table in ds.Tables) //1
+            {
+                foreach (DataRow row in table.Rows) //num of rows
+                {
+                    
+                    HtmlGenericControl rowDiv = new HtmlGenericControl("DIV");
+                    LinkButton rowBtnDelete = new LinkButton();
+                    LinkButton rowBtnEdit = new LinkButton();
+                    rowBtnEdit.Attributes.Add("class", "className");
+                    rowBtnEdit.Text = "Edit" + " ";
+                    rowBtnDelete.Text = "Delete";
+                    string rowSId = row["SId"].ToString();
+                    string rowName = row["Name"].ToString();
+                    string rowEmail = row["Email"].ToString();
+                    HtmlGenericControl itemspan = new HtmlGenericControl("SPAN");
+                    itemspan.InnerHtml = rowSId;
+                    itemspan.Style.Add("Width", "10%");
+                    HtmlGenericControl itemspan2 = new HtmlGenericControl("SPAN");
+                    itemspan2.InnerHtml = rowName;
+                    itemspan2.Style.Add("Width", "20%");
+                    HtmlGenericControl itemspan3 = new HtmlGenericControl("SPAN");
+                    itemspan3.InnerHtml = rowEmail;
+                    itemspan3.Style.Add("Width", "20%");
+
+                    rowDiv.Controls.Add(itemspan);
+                    rowDiv.Controls.Add(itemspan2);
+                    rowDiv.Controls.Add(itemspan3);
+
+                    rowDiv.Attributes.Add("class", "rowDiv");
+
+
+                    void edit_btn_Click(object sender, EventArgs s)
+                    {
+                        conn.Open();
+                        cmd.Connection = conn;
+                        cmd.CommandType = CommandType.Text;
+
+                        System.Diagnostics.Debug.WriteLine(rowSId + rowName + rowEmail);
+                        sIDTextBox.Text = rowSId;
+                        nameTextBox.Text = rowName;
+                        emailTextBox.Text = rowEmail;
+
+
+                        load_grid_view();
+                    }
+
+                    void delete_btn_Click(object sender, EventArgs e)
+                    {
+                        conn.Open();
+                        cmd.Connection = conn;
+                        cmd.CommandType = CommandType.Text;
+
+                        cmd.CommandText = "delete from student where sid = '" + rowSId + "'";
+                        cmd.ExecuteNonQuery();
+                        load_grid_view();
+                    }
+
+                    rowBtnEdit.Click += new EventHandler(edit_btn_Click);
+                    rowBtnDelete.Click += new EventHandler(delete_btn_Click);
+
+                    rowDiv.Controls.Add(rowBtnEdit);
+                    rowDiv.Controls.Add(rowBtnDelete);
+                    viewList.Controls.Add(rowDiv);
+                }
+            }
+        }
+
+        protected void addBtn_Click(object sender, EventArgs e)
+        {
+
+            //string id = TextBoxId.Text;
+            string name = nameTextBox.Text;
+            string email = emailTextBox.Text;
+
+            if (name == "" || email == "")
+            {
+                MessageBox.Show("You have a blank field");
+                return;
+            }
+
+            string query1 = "insert into STUDENT (sid, name, email) values ((SELECT MAX( sId )+1 FROM STUDENT),'" + name + "','" + email + "')";
+
+            conn.Open();
+            OracleCommand cmd = conn.CreateCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+
+            cmd.CommandText = query1;
+            cmd.ExecuteNonQuery();
+
+            if (cmd.Connection.State != ConnectionState.Closed)
+                cmd.Connection.Close();
+
+            load_grid_view();
+
+            sIDTextBox.Text = "";
+            nameTextBox.Text = "";
+            emailTextBox.Text = "";
+        }
+
+        protected void updateBtn_Click(object sender, EventArgs e)
+        {
+            string id = sIDTextBox.Text;
+            string name = nameTextBox.Text;
+            string email = emailTextBox.Text;
+            if (name == "" && email == "")
+            {
+                //ShowMessage("The field cannot be empty", "Check Field");
+                MessageBox.Show("Both fields are empty");
+                return;
+            }
+            if (name == "" || email == "")
+            {
+                //ShowMessage("The field cannot be empty", "Check Field");
+                MessageBox.Show("One of the field cannot be empty");
+                return;
+            }
+
+            string query = string.Format("UPDATE STUDENT SET Name = '{0}', Email = '{1}' WHERE sId = '{2}'", nameTextBox.Text, emailTextBox.Text, sIDTextBox.Text);
+
+            conn.Open();
+            OracleCommand cmd = conn.CreateCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = query;
+
+            cmd.ExecuteNonQuery();
+
+            if (cmd.Connection.State != ConnectionState.Closed)
+                cmd.Connection.Close();
+
+            load_grid_view();
+
+            sIDTextBox.Text = "";
+            nameTextBox.Text = "";
+            emailTextBox.Text = "";
+        }
+
+        private void ShowMessage(string Message, string Title)
+        {
+            ScriptManager.RegisterStartupScript(Page, this.GetType(), "alert", string.Format("alert('{1}', '{0}');", Message, Title), true);
+        }
+    }
+}
